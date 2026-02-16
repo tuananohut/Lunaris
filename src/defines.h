15665185d2f4fcf7724b36db55feb92e3d3f238c
Tuana Nohut <tkanesrkajol58@gmail.com>
2026-02-16 22:15:26 +0300

    fixing warnings

--- src/defines.h
+++ src/defines.h
@@ -24,7 +24,7 @@ struct Line
 {
   Point start_point;
   Point end_point; 
-};
+}; 
 
 void draw_line(Point start_point, Point end_point, TGAImage &framebuffer, TGAColor color)
 {
@@ -41,15 +41,44 @@ void draw_line(Point start_point, Point end_point, TGAImage &framebuffer, TGACol
       std::swap(start_point.y, end_point.y); 
     }
 
-  float y = start_point.y; 
+  float slope = (float)(end_point.y - start_point.y) / float(end_point.x - start_point.x); 
+  float y = (float)start_point.y; 
   for (int x = start_point.x; x < end_point.x; x++)
     {
       if (steep)
-	framebuffer.set(y, x, color);
+	framebuffer.set((int)y, x, color);
       else 
-	framebuffer.set(x, y, color);
+	framebuffer.set(x, (int)y, color);
 
-      y += (end_point.y - start_point.y) / float(end_point.x - start_point.x); 
+      y += slope; 
+    }
+} 
+
+void draw_line(Line line, TGAImage &framebuffer, TGAColor color)
+{
+  bool steep = std::abs(line.start_point.x - line.end_point.x) < std::abs(line.start_point.y - line.end_point.y);
+  if (steep)
+    {
+      std::swap(line.start_point.x, line.start_point.y);
+      std::swap(line.end_point.x,   line.end_point.y); 
+    }
+  
+  if (line.start_point.x > line.end_point.x)
+    {
+      std::swap(line.start_point.x, line.end_point.x);
+      std::swap(line.start_point.y, line.end_point.y); 
+    }
+
+  float slope = (float)(end_point.y - start_point.y) / float(end_point.x - start_point.x); 
+  float y = (float)line.start_point.y; 
+  for (int x = line.start_point.x; x < line.end_point.x; x++)
+    {
+      if (steep)
+	framebuffer.set((int)y, x, color);
+      else 
+	framebuffer.set(x, (int)y, color);
+
+      y += slope; 
     }
 } 
 
